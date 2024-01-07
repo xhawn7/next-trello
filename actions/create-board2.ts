8d6362e07ba48816e1ebb70d60822c08844b427b
xhawn <swanlz1992@gmail.com>
2024-01-07 17:22:21 +0800

    fix:hide-smth

--- actions/create-board2.ts
+++ actions/create-board2.ts
@@ -1,51 +1,51 @@
-'use server'
-import { db } from '@/lib/db'
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
-import { z } from 'zod'
+// 'use server'
+// import { db } from '@/lib/db'
+// import { revalidatePath } from 'next/cache'
+// import { redirect } from 'next/navigation'
+// import { z } from 'zod'
 
-export type State = {
-  message?: string | null
-  errors?: {
-    title?: string[]
-  }
-}
+// export type State = {
+//   message?: string | null
+//   errors?: {
+//     title?: string[]
+//   }
+// }
 
-const CreateBoard = z.object({
-  // title: z.string()
-  title: z.string().min(3, {
-    message: '必须超过3位字符'
-  })
-})
-// export async function created(formData: FormData) {
-export async function created(prev: State, formData: FormData) {
-  // const title = formData.get('title') as string
-  // const { title } = CreateBoard.parse({
-  const validatedFields = CreateBoard.safeParse({
-    title: formData.get('title')
-  })
+// const CreateBoard = z.object({
+//   // title: z.string()
+//   title: z.string().min(3, {
+//     message: '必须超过3位字符'
+//   })
+// })
+// // export async function created(formData: FormData) {
+// export async function created(prev: State, formData: FormData) {
+//   // const title = formData.get('title') as string
+//   // const { title } = CreateBoard.parse({
+//   const validatedFields = CreateBoard.safeParse({
+//     title: formData.get('title')
+//   })
 
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: 'missing fields'
-    }
-  }
+//   if (!validatedFields.success) {
+//     return {
+//       errors: validatedFields.error.flatten().fieldErrors,
+//       message: 'missing fields'
+//     }
+//   }
 
-  const { title } = validatedFields.data
+//   const { title } = validatedFields.data
 
-  try {
-    await db.board.create({
-      data: {
-        title
-      }
-    })
-  } catch (error) {
-    return {
-      message: 'database error'
-    }
-  }
+//   try {
+//     await db.board.create({
+//       data: {
+//         title
+//       }
+//     })
+//   } catch (error) {
+//     return {
+//       message: 'database error'
+//     }
+//   }
 
-  revalidatePath('/organization/org_2Zn06TVwcvvJiST7iw0gQerVNHa')
-  redirect('/organization/org_2Zn06TVwcvvJiST7iw0gQerVNHa')
-}
+//   revalidatePath('/organization/org_2Zn06TVwcvvJiST7iw0gQerVNHa')
+//   redirect('/organization/org_2Zn06TVwcvvJiST7iw0gQerVNHa')
+// }
--- actions/delete-board2.ts
+++ actions/delete-board2.ts
@@ -1,12 +1,12 @@
-'use server'
-import { db } from '@/lib/db'
-import { revalidatePath } from 'next/cache'
+// 'use server'
+// import { db } from '@/lib/db'
+// import { revalidatePath } from 'next/cache'
 
-export async function deleted(id: string) {
-  await db.board.delete({
-    where: {
-      id
-    }
-  })
-  revalidatePath('/organization/org_2Zn06TVwcvvJiST7iw0gQerVNHa')
-}
+// export async function deleted(id: string) {
+//   await db.board.delete({
+//     where: {
+//       id
+//     }
+//   })
+//   revalidatePath('/organization/org_2Zn06TVwcvvJiST7iw0gQerVNHa')
+// }
